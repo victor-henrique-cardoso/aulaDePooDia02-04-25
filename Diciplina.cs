df199a59547d0b66db93fe56e3a513d878600558
Victor Henrique Ferreira Cardoso <2024102020050@aluno.ifro.edu.br>
2025-04-10 14:59:54 -0400

    Comecei a fazer a classe Disciplina

--- Diciplina.cs
+++ Diciplina.cs
@@ -3,5 +3,27 @@ using static System.Console;
 using static System.Convert;
 
 class Diciplina{
+    private string nomedaDiciplina, codigoDaDiciplina, descricaoEmenta, areaConhecimento;
     
+    private int cargaHoraria, anoSerieRecomentadada;
+
+    public string GetNomeDiciplina(){
+        return this.nomedaDiciplina;
+    }
+    public void SetNomeDiciplina(string nomedaDiciplina)
+    {
+        this.nomedaDiciplina = nomedaDiciplina;
+    }
+    public string GetCodigoDiciplina(){
+        return this.codigoDaDiciplina;
+    }
+    public void SetCodigoDiciplina(string codigoDaDiciplina){
+        this.codigoDaDiciplina = codigoDaDiciplina;
+    }
+    public string GetDescricaoEmenta (){
+        return this.descricaoEmenta;
+    }
+    
+
+
 }
--- obj/Debug/net8.0/aulaclass.AssemblyInfo.cs
+++ obj/Debug/net8.0/aulaclass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aulaclass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486f4486cf702a1b01081052bf60cf8d731298a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4623ba2abccd9240da184c9429a8cc2bcf301797")]
 [assembly: System.Reflection.AssemblyProductAttribute("aulaclass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aulaclass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
